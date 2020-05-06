2214b76e2a70f55c19371e937d03fbaa6ac23947
JAEYONG CHOI <richson1@naver.com>
2020-05-06 21:21:51 +0900

    change https setting

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const port = process.env.PORT || 5000;
     EXPRESS CONFIGURATION
 ==========================*/
 const app = express();
+
+// cloudflare의 네트워크 속도문제로 우선 https 해제
+/*
 const https = require('https');
 const fs = require('fs');
 
@@ -23,7 +26,7 @@ const options = {
     key: fs.readFileSync('ssl/private.pem', 'utf8'),
     cert: fs.readFileSync('ssl/crt.pem', 'utf8'),
 }
-
+*/
 app.use(cors());
 // parse JSON and url-encoded query
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +36,12 @@ app.use(bodyParser.json());
 app.use('/api', require('./api/routes'));
 
 // open the server
-
+// cloudflare의 네트워크 속도문제로 우선 https 해제
+/*
 https.createServer(options, app).listen(port, () => {
+    console.log(`Express is running on port ${port}`);
+});
+*/
+app.listen(port, () => {
     console.log(`Express is running on port ${port}`);
 });
